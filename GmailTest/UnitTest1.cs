f8295ce093e2566b8a6ebcbd30919d0af64a32f6
Yevhenii <mihaylovskiyeugeny@gmail.com>
2021-08-02 10:34:38 +0300

    Add try/catch to UnitTest1
    Add try/catch to UnitTest1

--- GmailTest/UnitTest1.cs
+++ GmailTest/UnitTest1.cs
@@ -14,42 +14,61 @@ namespace GmailTest
         [SetUp]
         public void Setup()
         {
-            ChromeOptions options = new ChromeOptions();
-            options.AddArguments("--disable-notifications");
-            Driver = new ChromeDriver(options);
-            Driver.Navigate().GoToUrl(Variables.mainPageUrl);
-            Driver.Manage().Window.Maximize();
 
-            LoginFlow loginFlow = new();
-            loginFlow.LoginToAccount(Variables.secondUserEmail, Variables.firstAndSecondUserPassword);
+            try
+            {
+                ChromeOptions options = new ChromeOptions();
+                options.AddArguments("--disable-notifications");
+                Driver = new ChromeDriver(options);
+                Driver.Navigate().GoToUrl(Variables.mainPageUrl);
+                Driver.Manage().Window.Maximize();
 
-            NumberOfEmailFlow numberOfEmail = new();
-            numberOfEmailsBefore = numberOfEmail.ReturnNumberOfEmails();
+                LoginFlow loginFlow = new();
+                loginFlow.LoginToAccount(Variables.secondUserEmail, Variables.firstAndSecondUserPassword);
 
-            LogOutFlow logOutFlow = new();
-            logOutFlow.LogOut();
+                NumberOfEmailFlow numberOfEmail = new();
+                numberOfEmailsBefore = numberOfEmail.ReturnNumberOfEmails();
+
+                LogOutFlow logOutFlow = new();
+                logOutFlow.LogOut();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            
             
         }
 
         [Test]
         public void Test1()
         {
+            try
+            {
+                LoginFlow loginFlow = new();
+                loginFlow.LoginToAccount(Variables.firstUserEmail, Variables.firstAndSecondUserPassword);
+
+                SendEmailFlow sendEmailFlow = new();
+                sendEmailFlow.SendEmail(Variables.secondUserEmail, Variables.emailTheme, Variables.emailText);
+
+                LogOutFlow logOutFlow = new();
+                logOutFlow.LogOut();
+
+                loginFlow.LoginToAccount(Variables.secondUserEmail, Variables.firstAndSecondUserPassword);
+
+                NumberOfEmailFlow numberOfEmail = new();
+                numberOfEmailsAfter = numberOfEmail.ReturnNumberOfEmails();
+
+                Assert.Less(numberOfEmailsBefore, numberOfEmailsAfter);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Driver.Close();
+            }
             
-            LoginFlow loginFlow = new();
-            loginFlow.LoginToAccount(Variables.firstUserEmail, Variables.firstAndSecondUserPassword);
-
-            SendEmailFlow sendEmailFlow = new();
-            sendEmailFlow.SendEmail(Variables.secondUserEmail, Variables.emailTheme, Variables.emailText);
-
-            LogOutFlow logOutFlow = new();
-            logOutFlow.LogOut();
-
-            loginFlow.LoginToAccount(Variables.secondUserEmail, Variables.firstAndSecondUserPassword);
-
-            NumberOfEmailFlow numberOfEmail = new();
-            numberOfEmailsAfter = numberOfEmail.ReturnNumberOfEmails();
-
-            Assert.Less(numberOfEmailsBefore, numberOfEmailsAfter);
             
         }
 
